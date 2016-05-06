722125e48ea06dda3b526a2f392d6add6ebce512
Nirjal Khadka <nirjal@gmail.com>
2016-05-06 15:00:58 +0200

    added route for the api

--- server.js
+++ server.js
@@ -2,15 +2,30 @@ var express = require('express');
 var morgan = require('morgan');
 var bodyParser = require('body-parser');
 var app = express();
+var port = 7070;
 
 app.use(express.static(__dirname+'/public'));
 app.use(morgan('dev'));
+
+//Configure body parse to enable get data from POST
 app.use(bodyParser.urlencoded({'extended':'true'}));
 app.use(bodyParser.json());
 app.use(bodyParser.json({type:'applicatin/vnd.api+json'}));
 
+// ROUTES FOR OUR API
+// =============================================================================
+var router = express.Router();
+
+//test the route to make sure evreything is going fine
+
+router.get('/', function(req,res){
+    res.json({message: 'Welcome!, this message comes from the api'});
+});
+
+app.use('/api', router);
+
 app.get('*', function(req,res){
     res.sendFile('./public/index.html');
-})
-app.listen(7070);
-console.log("App listening on 7070");
+}); 
+app.listen(port);
+console.log("App listening port "+port);
